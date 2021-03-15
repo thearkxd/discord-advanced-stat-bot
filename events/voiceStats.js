97bbc4eb50e50cd3968bf8834c43f485fc9ad0bc
Theark <59927799+thearkxd@users.noreply.github.com>
2021-03-15 17:46:18 +0300

    Eski rankın rolünü almama sorunu giderildi.

---
+++ events/voiceStats.js
@@ -0,0 +1,59 @@
+const conf = require("../configs/config.json");
+const joinedAt = require("../schemas/voiceJoinedAt");
+const voiceUser = require("../schemas/voiceUser");
+const voiceGuild = require("../schemas/voiceGuild");
+const guildChannel = require("../schemas/voiceGuildChannel");
+const userChannel = require("../schemas/voiceUserChannel");
+const userParent = require("../schemas/voiceUserParent");
+const { MessageEmbed } = require("discord.js");
+const coin = require("../schemas/coin");
+const client = global.client;
+
+module.exports = async (oldState, newState) => {
+  if ((oldState.member && oldState.member.user.bot) || (newState.member && newState.member.user.bot)) return;
+  
+  if (!oldState.channelID && newState.channelID) await joinedAt.findOneAndUpdate({ userID: newState.id }, { $set: { date: Date.now() } }, { upsert: true });
+
+  let joinedAtData = await joinedAt.findOne({ userID: oldState.id });
+
+  if (!joinedAtData) await joinedAt.findOneAndUpdate({ userID: oldState.id }, { $set: { date: Date.now() } }, { upsert: true });
+  joinedAtData = await joinedAt.findOne({ userID: oldState.id });
+  const data = Date.now() - joinedAtData.date;
+
+  if (oldState.channelID && !newState.channelID) {
+    await saveDatas(oldState, oldState.channel, data);
+    await joinedAt.deleteOne({ userID: oldState.id });
+  } else if (oldState.channelID && newState.channelID) {
+    await saveDatas(oldState, oldState.channel, data);
+    await joinedAt.findOneAndUpdate({ userID: oldState.id }, { $set: { date: Date.now() } }, { upsert: true });
+  }
+};
+
+async function saveDatas(user, channel, data) {
+  if (conf.staffs.some(x => user.member.roles.cache.has(x))) {
+    if (channel.parent && conf.publicParents.includes(channel.parentID)) {
+      if (data >= (1000 * 60) * conf.voiceCount) await coin.findOneAndUpdate({ guildID: user.guild.id, userID: user.id }, { $inc: { coin: conf.publicCoin * parseInt(data/1000/60) } }, { upsert: true });
+    } else if (data >= (1000 * 60) * conf.voiceCount) await coin.findOneAndUpdate({ guildID: user.guild.id, userID: user.id }, { $inc: { coin: conf.voiceCoin * parseInt(data/1000/60) } }, { upsert: true });
+    const coinData = await coin.findOne({ guildID: user.guild.id, userID: user.id });
+    if (coinData && client.ranks.some(x => x.coin >= coinData.coin)) {
+      let newRank = client.ranks.filter(x => coinData.coin >= x.coin);
+      newRank = newRank[newRank.length-1];
+      const oldRank = client.ranks[client.ranks.indexOf(newRank)-1];
+      console.log(newRank.role)
+      console.log(oldRank.role)
+      user.member.roles.add(newRank.role);
+      if (Array.isArray(oldRank.role) && oldRank.role.some(x => user.member.roles.cache.has(x)) || !Array.isArray(oldRank.role) && user.member.roles.cache.has(oldRank.role)) user.member.roles.remove(oldRank.role);
+      const embed = new MessageEmbed().setColor("GREEN");
+      user.guild.channels.cache.get(conf.rankLog).send(embed.setDescription(`${user.member.toString()} üyesi **${coinData.coin}** coin hedefine ulaştı ve ${Array.isArray(newRank.role) ? newRank.role.map(x => `<@&${x}>`).join(", ") : `<@&${newRank.role}>`} rolü verildi!`));
+    }
+  }
+  await voiceUser.findOneAndUpdate({ guildID: user.guild.id, userID: user.id }, { $inc: { topStat: data, dailyStat: data, weeklyStat: data, twoWeeklyStat: data } }, { upsert: true });
+  await voiceGuild.findOneAndUpdate({ guildID: user.guild.id }, { $inc: { topStat: data, dailyStat: data, weeklyStat: data, twoWeeklyStat: data } }, { upsert: true });
+  await guildChannel.findOneAndUpdate({ guildID: user.guild.id, channelID: channel.id }, { $inc: { channelData: data } }, { upsert: true });
+  await userChannel.findOneAndUpdate({ guildID: user.guild.id, userID: user.id, channelID: channel.id }, { $inc: { channelData: data } }, { upsert: true });
+  if (channel.parent) await userParent.findOneAndUpdate({ guildID: user.guild.id, userID: user.id, parentID: channel.parentID }, { $inc: { parentData: data } }, { upsert: true });
+}
+
+module.exports.conf = {
+  name: "voiceStateUpdate",
+};
