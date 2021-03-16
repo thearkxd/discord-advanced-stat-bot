855c0fbd5b0bdd355d980668f97cf00f839157cf
Theark <59927799+thearkxd@users.noreply.github.com>
2021-03-16 09:28:38 +0300

    Her mesajda messageCount arttırma sorunu düzeltildi.

--- src/events/messageStats.js
+++ src/events/messageStats.js
@@ -7,16 +7,18 @@ const userChannel = require("../schemas/messageUserChannel");
 const { MessageEmbed } = require("discord.js");
 const coin = require("../schemas/coin");
 const client = global.client;
-let num = 0;
+const nums = new Map();
 
 module.exports = async (message) => {
   if (message.author.bot || !message.guild || message.content.startsWith(settings.prefix)) return;
   
   if (conf.staffs.some(x => message.member.roles.cache.has(x))) {
-    if (num % conf.messageCount === 0) {
+    const num = nums.get(message.author.id);
+    if (num && (num % conf.messageCount) === 0) {
+      nums.set(message.author.id, num + 1);
       await coin.findOneAndUpdate({ guildID: message.guild.id, userID: message.author.id }, { $inc: { coin: conf.messageCoin } }, { upsert: true });
-      num++;
       const coinData = await coin.findOne({ guildID: message.guild.id, userID: message.author.id });
+      console.log(coinData.coin);
       if (coinData && client.ranks.some(x => coinData.coin === x.coin)) {
         let newRank = client.ranks.filter(x => coinData.coin >= x.coin);
         newRank = newRank[newRank.length-1];
@@ -26,7 +28,7 @@ module.exports = async (message) => {
         const embed = new MessageEmbed().setColor("GREEN");
         message.guild.channels.cache.get(conf.rankLog).send(embed.setDescription(`${message.member.toString()} üyesi **${coinData.coin}** coin hedefine ulaştı ve ${Array.isArray(newRank.role) ? newRank.role.map(x => `<@&${x}>`).join(", ") : `<@&${newRank.role}>`} rolü verildi!`));
       }
-    } else num++;
+    } else nums.set(message.author.id, num ? num + 1 : 1);
   }
 
   await messageUser.findOneAndUpdate({ guildID: message.guild.id, userID: message.author.id }, { $inc: { topStat: 1, dailyStat: 1, weeklyStat: 1, twoWeeklyStat: 1 } }, { upsert: true });
