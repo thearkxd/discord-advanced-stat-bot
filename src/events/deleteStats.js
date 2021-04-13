de7e49599d2071ef1332b13dbf37ae7d083f0b26
Theark <59927799+thearkxd@users.noreply.github.com>
2021-04-13 17:05:54 +0300

    Update deleteStats.js

--- src/events/deleteStats.js
+++ src/events/deleteStats.js
@@ -4,83 +4,34 @@ const messageGuild = require("../schemas/messageGuild");
 const voiceGuild = require("../schemas/voiceGuild");
 const messageUser = require("../schemas/messageUser");
 const voiceUser = require("../schemas/voiceUser");
-let Week = 0;
 
-module.exports = async () => {
-  const daily = new CronJob("00 00 * * *", async () => {
+module.exports = () => {
+  new CronJob("0 0 * * *", () => {
     client.guilds.cache.forEach(async (guild) => {
-      const messageGuildData = await messageGuild.findOne({ guildID: guild.id });
-      const voiceGuildData = await voiceGuild.findOne({ guildID: guild.id });
-      const messageUserData = await messageUser.findOne({ guildID: guild.id });
-      const voiceUserData = await voiceUser.findOne({ guildID: guild.id });
-
-      if (messageGuildData) {
-        messageGuildData.dailyStat = 0;
-        messageGuildData.save();
-      }
-      if (voiceGuildData) {
-        voiceGuildData.dailyStat = 0;
-        voiceGuildData.save();
-      }
-      if (messageUserData) {
-        messageUserData.dailyStat = 0;
-        messageUserData.save();
-      }
-      if (voiceUserData) {
-        voiceUserData.dailyStat = 0;
-        voiceUserData.save();
-      }
+      await messageGuild.findOneAndUpdate({ guildID: guild.id }, { $set: { dailyStat: 0 } });
+      await voiceGuild.findOneAndUpdate({ guildID: guild.id }, { $set: { dailyStat: 0 } });
+      await messageUser.findOneAndUpdate({ guildID: guild.id }, { $set: { dailyStat: 0 } });
+      await voiceUser.findOneAndUpdate({ guildID: guild.id }, { $set: { dailyStat: 0 } });
     });
-  }, null, true, "Europe/Istanbul");
-  daily.start();
-
-  const weekly = new CronJob("00 00 * * 1", async () => {
-    Week += 1;
+  }, null, true, "Europe/Istanbul").start();
+  
+  new CronJob("0 0 * * 0", () => {
     client.guilds.cache.forEach(async (guild) => {
-      const messageGuildData = await messageGuild.findOne({ guildID: guild.id });
-      const voiceGuildData = await voiceGuild.findOne({ guildID: guild.id });
-      const messageUserData = await messageUser.findOne({ guildID: guild.id });
-      const voiceUserData = await voiceUser.findOne({ guildID: guild.id });
-
-      if (messageGuildData) {
-        messageGuildData.weeklyStat = 0;
-        messageGuildData.save();
-      }
-      if (voiceGuildData) {
-        voiceGuildData.weeklyStat = 0;
-        voiceGuildData.save();
-      }
-      if (messageUserData) {
-        messageUserData.weeklyStat = 0;
-        messageUserData.save();
-      }
-      if (voiceUserData) {
-        voiceUserData.weeklyStat = 0;
-        voiceUserData.save();
-      }
-
-      if (Week === 2) {
-        Week = 0;
-        if (messageGuildData) {
-          messageGuildData.twoWeeklyStat = 0;
-          messageGuildData.save();
-        }
-        if (voiceGuildData) {
-          voiceGuildData.twoWeeklyStat = 0;
-          voiceGuildData.save();
-        }
-        if (messageUserData) {
-          messageUserData.twoWeeklyStat = 0;
-          messageUserData.save();
-        }
-        if (voiceUserData) {
-          voiceUserData.twoWeeklyStat = 0;
-          voiceUserData.save();
-        }
-      }
+      await messageGuild.findOneAndUpdate({ guildID: guild.id }, { $set: { weeklyStat: 0 } });
+      await voiceGuild.findOneAndUpdate({ guildID: guild.id }, { $set: { weeklyStat: 0 } });
+      await messageUser.findOneAndUpdate({ guildID: guild.id }, { $set: { weeklyStat: 0 } });
+      await voiceUser.findOneAndUpdate({ guildID: guild.id }, { $set: { weeklyStat: 0 } });
     });
-  }, null, true, "Europe/Istanbul");
-  weekly.start();
+  }, null, true, "Europe/Istanbul").start();
+
+  new CronJob("0 0 1,15 * *", () => {
+    client.guilds.cache.forEach(async (guild) => {
+      await messageGuild.findOneAndUpdate({ guildID: guild.id }, { $set: { twoWeeklyStat: 0 } });
+      await voiceGuild.findOneAndUpdate({ guildID: guild.id }, { $set: { twoWeeklyStat: 0 } });
+      await messageUser.findOneAndUpdate({ guildID: guild.id }, { $set: { twoWeeklyStat: 0 } });
+      await voiceUser.findOneAndUpdate({ guildID: guild.id }, { $set: { twoWeeklyStat: 0 } });
+    });
+  }, null, true, "Europe/Istanbul").start();
 };
 
 module.exports.conf = {
