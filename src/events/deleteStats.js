f2cebb9505af5306a20def3a421b9987545385b4
Theark <59927799+thearkxd@users.noreply.github.com>
2021-04-28 18:29:57 +0300

    Günlük, haftalık ve 2 haftalık verilerin silinmeme sorunu düzeltildi!

--- src/events/deleteStats.js
+++ src/events/deleteStats.js
@@ -6,34 +6,37 @@ const messageUser = require("../schemas/messageUser");
 const voiceUser = require("../schemas/voiceUser");
 
 module.exports = () => {
-  new CronJob("0 0 * * *", () => {
+  const daily = new CronJob("0 0 * * *", () => {
     client.guilds.cache.forEach(async (guild) => {
       await messageGuild.findOneAndUpdate({ guildID: guild.id }, { $set: { dailyStat: 0 } });
       await voiceGuild.findOneAndUpdate({ guildID: guild.id }, { $set: { dailyStat: 0 } });
       await messageUser.findOneAndUpdate({ guildID: guild.id }, { $set: { dailyStat: 0 } });
       await voiceUser.findOneAndUpdate({ guildID: guild.id }, { $set: { dailyStat: 0 } });
     });
-  }, null, true, "Europe/Istanbul").start();
-  
-  new CronJob("0 0 * * 0", () => {
+  }, null, true, "Europe/Istanbul");
+  daily.start();
+
+  const weekly = new CronJob("0 0 * * 0", () => {
     client.guilds.cache.forEach(async (guild) => {
       await messageGuild.findOneAndUpdate({ guildID: guild.id }, { $set: { weeklyStat: 0 } });
       await voiceGuild.findOneAndUpdate({ guildID: guild.id }, { $set: { weeklyStat: 0 } });
       await messageUser.findOneAndUpdate({ guildID: guild.id }, { $set: { weeklyStat: 0 } });
       await voiceUser.findOneAndUpdate({ guildID: guild.id }, { $set: { weeklyStat: 0 } });
     });
-  }, null, true, "Europe/Istanbul").start();
+  }, null, true, "Europe/Istanbul");
+  weekly.start();
 
-  new CronJob("0 0 1,15 * *", () => {
+  const twoWeekly = new CronJob("0 0 1,15 * *", () => {
     client.guilds.cache.forEach(async (guild) => {
       await messageGuild.findOneAndUpdate({ guildID: guild.id }, { $set: { twoWeeklyStat: 0 } });
       await voiceGuild.findOneAndUpdate({ guildID: guild.id }, { $set: { twoWeeklyStat: 0 } });
       await messageUser.findOneAndUpdate({ guildID: guild.id }, { $set: { twoWeeklyStat: 0 } });
       await voiceUser.findOneAndUpdate({ guildID: guild.id }, { $set: { twoWeeklyStat: 0 } });
     });
-  }, null, true, "Europe/Istanbul").start();
+  }, null, true, "Europe/Istanbul")
+  twoWeekly.start();
 };
 
 module.exports.conf = {
-  name: "ready",
+  name: "ready"
 };
