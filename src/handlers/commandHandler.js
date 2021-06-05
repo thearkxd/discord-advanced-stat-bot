804d4f054457799589ea53301a5bcee4819b2c90
AlperTheArk <59927799+AlperTheArk@users.noreply.github.com>
2021-06-05 10:32:47 +0300

    Komutla rank ekleyince rank sırası bozulma sorunu giderildi!

--- src/handlers/commandHandler.js
+++ src/handlers/commandHandler.js
@@ -2,10 +2,10 @@ const fs = require("fs");
 const client = global.client;
 
 fs.readdir("./src/commands", (err, files) => {
-  if (err) console.error(err);
-  files.forEach((f) => {
-    const props = require(`../commands/${f}`);
-    console.log(`[COMMAND] ${props.conf.name} loaded!`);
-    client.commands.set(props.conf.name, props);
-  });
+	if (err) console.error(err);
+	files.forEach((f) => {
+		const props = require(`../commands/${f}`);
+		console.log(`[COMMAND] ${props.conf.name} loaded!`);
+		client.commands.set(props.conf.name, props);
+	});
 });
--- theark.js
+++ theark.js
@@ -6,7 +6,7 @@ global.confdb = new Database("./src/configs/config.json");
 const rankdb = global.rankdb = new Database("./src/configs/ranks.json");
 client.commands = new Collection();
 client.cooldown = new Map();
-client.ranks = rankdb.get("ranks") ? rankdb.get("ranks").sort((a, b) => b.coin - a.coin) : [];
+client.ranks = rankdb.get("ranks") ? rankdb.get("ranks").sort((a, b) => a.coin - b.coin) : [];
 client.tasks = rankdb.get("tasks") || [];
 require("./src/handlers/commandHandler");
 require("./src/handlers/eventHandler");
