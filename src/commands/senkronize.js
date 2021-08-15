c483026cdf88be7bfe2e0c282b4ec3b65a37ba6d
Theark <59927799+thearkxd@users.noreply.github.com>
2021-08-15 15:20:56 +0300

    Rol gösterme sorunu düzeltildi.

--- src/commands/senkronize.js
+++ src/commands/senkronize.js
@@ -26,7 +26,7 @@ module.exports = {
         let rank = client.ranks.filter(x => member.hasRole(x.role));
         rank = rank[rank.length-1];
         await coin.findOneAndUpdate({ guildID: message.guild.id, userID: member.user.id }, { $set: { coin: rank.coin } }, { upsert: true });
-        message.channel.send(embed.setDescription(`${member.toString()} üyesinde <@&${rank.role}> rolü bulundu ve coini ${rank.coin} olarak değiştirildi!`));
+        message.channel.send(embed.setDescription(`${member.toString()} üyesinde ${Array.isArray(rank.role) ? rank.role.map(x => `<@&${x}>`).join(", ") : `<@&${rank.role}>`} rolü bulundu ve coini ${rank.coin} olarak değiştirildi!`));
       } else return message.channel.send(embed.setDescription(`${member.toString()} üyesinde sistemde ayarlı bir rol bulunamadı!`));
     } else if (args[0] === "role" || args[0] === "rol") {
       const role = message.mentions.roles.first() || message.guild.roles.cache.get(args[1]);
@@ -38,7 +38,7 @@ module.exports = {
           let rank = client.ranks.filter(x => member.hasRole(x.role));
           rank = rank[rank.length-1];
           await coin.findOneAndUpdate({ guildID: message.guild.id, userID: member.user.id }, { $set: { coin: rank.coin } }, { upsert: true });
-          message.channel.send(embed.setDescription(`${member.toString()} üyesinde <@&${rank.role}> rolü bulundu ve coini ${rank.coin} olarak değiştirildi!`));
+          message.channel.send(embed.setDescription(`${member.toString()} üyesinde ${Array.isArray(rank.role) ? rank.role.map(x => `<@&${x}>`).join(", ") : `<@&${rank.role}>`} rolü bulundu ve coini ${rank.coin} olarak değiştirildi!`));
         } else return message.channel.send(embed.setDescription(`${member.toString()} üyesinde sistemde ayarlı bir rol bulunamadı!`));
       });
     } else return message.channel.send(embed.setDescription("Bir argüman belirtmelisin!"));
